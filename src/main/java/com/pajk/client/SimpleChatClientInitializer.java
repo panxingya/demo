6fcfac2c8cb70c3ee0a1366a78eaa0c649365e25
panxingya <pxy1989123@126.com>
2016-05-03 17:05:55 +0800

    add tcp server and client

---
+++ src/main/java/com/pajk/client/SimpleChatClientInitializer.java
@@ -0,0 +1,25 @@
+package com.pajk.client;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+/**
+ * Created by panxingya on 2016/5/3.
+ */
+public class SimpleChatClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast("handler", new SimpleChatClientHandler());
+    }
+}
